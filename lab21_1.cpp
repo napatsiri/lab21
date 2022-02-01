343da77b8280c9738ca6512f680f5dcdab723eb1
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2022-02-01 16:31:49 +0700

    lab21_1.cpp

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,16 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){ 
+	double max_X = max(R1.x,R2.x); 
+	double min_X = min(R1.x+R1.w,R2.x+R2.w); 
+	double min_Y = min(R1.y,R2.y); 
+	double max_Y = max(R1.x-R1.h,R2.x-R2.h); 
+	double overlap_X = min_X-max_X; 
+	double overlap_Y = min_Y-max_Y; 
 
-
-
+	if(overlap_X <= 0 || overlap_Y <= 0) 
+	
+return 0; 
+return overlap_X*overlap_Y; 
 }
